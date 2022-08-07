ecd7c188620f0f012be2635d4e187e130d894a98
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-08-07 20:42:15 -0300

    feat: ensure return bad request if invalid mail is provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,8 +1,12 @@
-import { HttpRequest, HttpResponse, Controller } from '../contracts'
-import { MissingParamError } from '../errors'
+import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../contracts'
+import { InvalidParamError, MissingParamError } from '../errors'
 import { badRequest } from '../helpers/badRequest'
 
 export class SignUpController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
@@ -10,6 +14,9 @@ export class SignUpController implements Controller {
 
       return badRequest(new MissingParamError(field))
     }
+
+    const { email } = httpRequest.body
+    if (!this.emailValidator.isValid(email)) return badRequest(new InvalidParamError('email'))
 
     return {
       statusCode: 201,
