61d8897f1c9273b3bba893dcd7e3755fcfd4c559
Carlos Vinicius <viniciusdev.26@gmail.com>
2022-08-07 01:48:50 -0300

    feat: ensure SignUpController return email an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,9 +2,18 @@ import { MissingParamError } from '../errors/missing-param-error'
 
 export class SignUpController {
   handle (httpRequest: any): any {
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (httpRequest.body[field]) continue
+
+      return {
+        statusCode: 400,
+        body: new MissingParamError(field)
+      }
+    }
+
     return {
-      statusCode: 400,
-      body: new MissingParamError('name')
+      statusCode: 200
     }
   }
 }
